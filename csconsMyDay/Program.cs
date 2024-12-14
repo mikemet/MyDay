303d981e2740b3e86924f5c7b2e1978f72496565
mikemet <mikemet@ya.ru>
2024-12-14 21:20:37 +0300

    adding code

--- csconsMyDay/Program.cs
+++ csconsMyDay/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string? input;
+var rand = new Random();
+int days;
+
+enter:
+do
+{
+    Console.WriteLine("Enter the CORRECT NUMBER of days of the month: ");
+    input = Console.ReadLine();
+} while (int.TryParse(input, out days) == false);
+
+if (days < 28 || days >31)
+{
+    goto enter;
+}
+
+List<int> lstDays = [..Enumerable.Range(1, days)];
+var day = rand.Next(lstDays.Count);
+
+Console.WriteLine($"Your day is: {day}");
