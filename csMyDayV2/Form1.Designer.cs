35e9921de6ae8d6701feaa448538d25d0d545a10
mikemet <mikemet@ya.ru>
2024-12-18 22:28:54 +0300

    adding common code

--- csMyDayV2/Form1.Designer.cs
+++ csMyDayV2/Form1.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            comboBox1 = new ComboBox();
+            button1 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Январь", "Февраль", "Февраль*29", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Ноябрь", "Декабрь" });
+            comboBox1.Location = new Point(12, 12);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(280, 29);
+            comboBox1.TabIndex = 0;
+            comboBox1.Text = "Выберите месяц";
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 81);
+            button1.Name = "button1";
+            button1.Size = new Size(280, 43);
+            button1.TabIndex = 1;
+            button1.Text = "Ваш ДЕНЬ!";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.Font = new Font("MV Boli", 48F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 174);
+            label1.Name = "label1";
+            label1.Size = new Size(158, 154);
+            label1.TabIndex = 2;
+            label1.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 346);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(comboBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private ComboBox comboBox1;
+        private Button button1;
+        private Label label1;
     }
 }
--- csMyDayV2/Form1.cs
+++ csMyDayV2/Form1.cs
@@ -6,5 +6,47 @@ namespace csMyDayV2
         {
             InitializeComponent();
         }
+
+        string? Month;
+        int days, day;
+        Random rand = new Random();
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Month = comboBox1.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Generate();
+            label1.Text = Convert.ToString(day);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private int Generate()
+        {
+            if (Month == "Январь" || Month == "Март" || Month == "Май" || Month == "Июль" || Month == "Август" || Month == "Октябрь" || Month == "Декабрь")
+            {
+                days = 31;
+            }
+            else if (Month == "Апрель" || Month == "Июнь" || Month == "Сентябрь" || Month == "Ноябрь")
+            {
+                days = 30;
+            } else if (Month == "Февраль")
+            {
+                days = 28;
+            }
+            else days = 29;
+
+            List<int> ListDays = [..Enumerable.Range(1, days)];
+            day = rand.Next(ListDays.Count);
+
+            return day;
+        }
     }
 }
