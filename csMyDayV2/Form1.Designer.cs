35d1455fa0283347610c5ea324fc77609c82b490
mikemet <mikemet@ya.ru>
2024-12-20 19:57:50 +0300

    editing disagn

--- csMyDayV2/Form1.Designer.cs
+++ csMyDayV2/Form1.Designer.cs
@@ -28,53 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             comboBox1 = new ComboBox();
             button1 = new Button();
             label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // comboBox1
             // 
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
             comboBox1.FormattingEnabled = true;
             comboBox1.Items.AddRange(new object[] { "Январь", "Февраль", "Февраль*29", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Ноябрь", "Декабрь" });
-            comboBox1.Location = new Point(12, 12);
+            comboBox1.Location = new Point(12, 60);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(280, 29);
             comboBox1.TabIndex = 0;
-            comboBox1.Text = "Выберите месяц";
+            comboBox1.UseWaitCursor = true;
             comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
             // 
             // button1
             // 
-            button1.Location = new Point(12, 81);
+            button1.BackColor = Color.FromArgb(255, 224, 192);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 204);
+            button1.ForeColor = Color.Navy;
+            button1.Location = new Point(12, 129);
             button1.Name = "button1";
             button1.Size = new Size(280, 43);
             button1.TabIndex = 1;
             button1.Text = "Ваш ДЕНЬ!";
-            button1.UseVisualStyleBackColor = true;
+            button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
             // 
             // label1
             // 
+            label1.BackColor = Color.FromArgb(192, 255, 192);
             label1.BorderStyle = BorderStyle.FixedSingle;
-            label1.Font = new Font("MV Boli", 48F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(12, 174);
+            label1.Font = new Font("MV Boli", 72F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Purple;
+            label1.Location = new Point(134, 207);
             label1.Name = "label1";
             label1.Size = new Size(158, 154);
             label1.TabIndex = 2;
+            label1.TextAlign = ContentAlignment.MiddleCenter;
             label1.Click += label1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(12, 239);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(100, 79);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Italic, GraphicsUnit.Point, 204);
+            label2.ForeColor = Color.Navy;
+            label2.Location = new Point(12, 18);
+            label2.Name = "label2";
+            label2.Size = new Size(187, 30);
+            label2.TabIndex = 4;
+            label2.Text = "Выберите Месяц :";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(9F, 21F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 346);
+            BackColor = Color.FromArgb(224, 224, 224);
+            ClientSize = new Size(311, 378);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
             Controls.Add(label1);
             Controls.Add(button1);
             Controls.Add(comboBox1);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Твой ДЕНЬ!";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -82,5 +120,7 @@
         private ComboBox comboBox1;
         private Button button1;
         private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
     }
 }
